cba2f568692c1ed0779a8e69cbbcdb5264652cef
fernando.oliveira <fernando.oliveira@sou.inteli.edu.br>
2026-09-21 01:00:02 -0300

    melhora

--- esp32/socorro_detector/socorro_detector.ino
+++ esp32/socorro_detector/socorro_detector.ino
@@ -197,7 +197,7 @@ void setupI2S() {
     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
     .sample_rate = SR,
     .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,   // INMP441 c/ L/R=GND no ESP32: ler canal DIREITO
     .communication_format = I2S_COMM_FORMAT_STAND_I2S,
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
     .dma_buf_count = 8,
